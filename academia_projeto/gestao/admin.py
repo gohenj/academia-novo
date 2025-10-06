0a155c70863ab35916b96169c8febfffe893a7e2
gohenj <gusgustaca@gmail.com>
2025-10-06 09:43:16 -0300

    sla

--- academia_projeto/gestao/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Cidade, Professor, Aluno, Modalidade, Matricula
-
-admin.site.register(Cidade)
-admin.site.register(Professor)
-admin.site.register(Aluno)
-admin.site.register(Modalidade)
-admin.site.register(Matricula)
--- academia_projeto/gestao/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Quando o endereço for /gestao/alunos/, chame a função 'lista_alunos' da nossa view
-    path('alunos/', views.lista_alunos, name='lista_alunos'),
-]
